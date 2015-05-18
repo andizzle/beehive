af1612cc4ac19974cd916c3caa4725e52254f7a3
Andy Zhang <andizzle.zhang@gmail.com>
2015-05-18 17:10:58 +1000

    allow create hives

--- src/cockpit/hq.rb
+++ src/cockpit/hq.rb
@@ -4,11 +4,14 @@ class Hq
 
   @command = nil
   @options = {}
+  @hives = []
   @region = 'us-east-1d'
 
   def initialize(command, options={})
     @command = command
     @options = options
+    @region = options.has_key?(:region) ? options[:region] : @region
+    Aws.config.update({:region => @region})
   end
 
   def dispatch
@@ -18,7 +21,22 @@ class Hq
     end
   end
 
+  # create a number of hives using user options
   def createHives(options)
-    puts Aws.config
+    number_of_hive = options.has_key?(:number) ? options[:number].to_i : 1
+    ec2_general = Aws::EC2::Resource.new
+    hive_options = {
+      :image_id      => 'ami-cb49d7f1',
+      :min_count     => number_of_hive,
+      :max_count     => number_of_hive,
+      :instance_type => 't1.micro'
+    }
+    hive_options.merge!(options.select {|k,v| hive_options.has_key?(k)})
+    @hives = ec2_general.create_instances hive_options
+    ec2_general.create_tags({:tags => [{:key => 'Name', :value => 'hive'}], :resources => @hives.map(&:id)})
+  end
+
+  def destroyHives(options)
+
   end
 end
