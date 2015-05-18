bb0314725177e5244133870a4dcf59d51f90181a
Andy Zhang <andizzle.zhang@gmail.com>
2015-05-18 23:03:27 +1000

    Check new instances status upon create

--- src/cockpit/hq.rb
+++ src/cockpit/hq.rb
@@ -37,9 +37,34 @@ class Hq
     @@hives = ec2_general.create_instances hive_options
     puts "%i hives are being built" % number_of_hive
     ec2_general.create_tags({:tags => [{:key => 'Name', :value => 'hive'}], :resources => @@hives.map(&:id)})
+    checkHivesStatus
   end
 
   def destroyHives(options)
 
   end
+
+  private
+
+  # check over status of hives
+  def checkHivesStatus
+    ec2_control = Aws::EC2::Client.new
+    hives_built = []
+    filters = [{:name => 'instance-state-name', :values => ['pending', 'running']}]
+    while hives_built.count != @@hives.count do
+      statuses = ec2_control.describe_instance_status instance_ids: @@hives.map(&:id), include_all_instances: true, filters: filters
+      statuses.each do |response|
+        response[:instance_statuses].each do |instance|
+          building = instance[:instance_state].name == 'running' ? false : true
+          instance_id = instance[:instance_id]
+          if not building and not hives_built.include? instance_id
+            puts 'Hive %s is ready!' % instance_id
+            hives_built << instance_id
+          end
+        end
+      end
+      sleep(1)
+    end
+  end
+
 end
