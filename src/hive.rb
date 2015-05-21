8a2cc7577171ff5c264c3e7dc325dbf299207349
Andy Zhang <andizzle.zhang@gmail.com>
2015-05-22 09:17:46 +1000

    abstract all three commands out.

--- src/hive.rb
+++ src/hive.rb
@@ -21,41 +21,52 @@ module Fleet
 
       ::Net::SSH.start(@ip, @username, :keys => [@key]) do |ssh|
 
-        # add new ab command to ab.sh
-        benchmark_command = 'ab -r -n %{number} -c %{concurrent} "%{url}" >> /root/results/ab.out' % option
-        create_exec = ssh.open_channel do |cha|
-          cha.exec 'echo "%s" > /home/ubuntu/ab.sh' % benchmark_command do |ch, success|
-
-            raise "could not execute command" unless success
-
-          end
-        end
-
+        # prepare the attack
+        create_exec = _prepare ssh, option
         create_exec.wait
 
         # build the command
         #open a new channel and run the container
-        attack = _attack_command ssh, option
+        attack = _execute ssh, option
         attack.wait
 
         # remove all exited containers
-        ssh.open_channel do |cha|
-          cha.exec 'docker ps -aq -f status=exited | xargs docker rm' do |ch, success|
-            raise "could not execute command" unless success
-          end
-        end
+        _clean ssh
 
       end
 
       Fleet.report data
     end
 
-    def _attack_command(ssh, option)
+    private
+    # add new ab command to ab.sh
+    def _prepare(ssh, option)
+      benchmark_command = 'ab -r -n %{number} -c %{concurrent} "%{url}" >> /root/results/ab.out' % option
+      ssh.open_channel do |cha|
+        cha.exec 'echo "%s" > /home/ubuntu/ab.sh' % benchmark_command do |ch, success|
+
+          raise "could not execute command" unless success
+
+        end
+      end
+    end
+
+    # start the attack
+    def _execute(ssh, option)
       ssh.open_channel do |cha|
         cha.exec 'docker-compose -f /home/ubuntu/docker-compose.yml scale ab=%s' % option[:bees] do |ch, success|
 
           raise "could not execute command" unless success
 
+        end
+      end
+    end
+
+    # clean up the battlefield
+    def _clean(ssh)
+      ssh.open_channel do |cha|
+        cha.exec 'docker ps -aq -f status=exited | xargs docker rm' do |ch, success|
+          raise "could not execute command" unless success
         end
       end
     end
